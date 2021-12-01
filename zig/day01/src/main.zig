7be5d2ea1eeb617943e61a142fe205d7c98f87a9
Michael Kedzierski <michael.kedzierski@okendo.io>
2021-12-02 09:45:41 +1100

    day01: minor cleanups

--- zig/day01/src/main.zig
+++ zig/day01/src/main.zig
@@ -25,15 +25,8 @@ fn one(depths: []u32) anyerror!void {
     var increase_count: u32 = 0;
     var previous_depth: ?u32 = null;
     for (depths) |d| {
-        if (previous_depth == null) {
-            std.log.info("{d} (N/A)", .{d});
-        } else if (previous_depth.? < d) {
-            std.log.info("{d} (increased)", .{d});
+        if (should_increase(previous_depth, d)) {
             increase_count += 1;
-        } else if (previous_depth.? == d) {
-            std.log.info("{d} (same)", .{d});
-        } else {
-            std.log.info("{d} (decreased)", .{d});
         }
         previous_depth = d;
     }
@@ -45,7 +38,8 @@ fn two(depths: []u32) anyerror!void {
     var increase_count: u32 = 0;
     var previous_sum: ?u32 = null;
     var windows = [3]u32{ 0, 0, 0 };
-    for (depths) |d, i| {
+    var i: usize = 0;
+    while (true) : (i += 1) {
         if (i > 2) {
             const sum = windows[i % 3];
             if (should_increase(previous_sum, sum)) {
@@ -54,28 +48,27 @@ fn two(depths: []u32) anyerror!void {
             previous_sum = sum;
             windows[i % 3] = 0;
         }
+        if (i == depths.len) {
+            break;
+        }
         var wi: u8 = 0;
         while (wi < std.math.min(3, i + 1)) : (wi += 1) {
-            windows[wi] += d;
+            windows[wi] += depths[i];
         }
-    }
-    const sum = windows[depths.len % 3];
-    if (should_increase(previous_sum, sum)) {
-        increase_count += 1;
     }
     std.log.info("increase count: {any}", .{increase_count});
 }
 
-fn should_increase(previous_sum: ?u32, sum: u32) bool {
-    if (previous_sum == null) {
-        std.log.info("{any} (NA)", .{sum});
-    } else if (previous_sum.? < sum) {
-        std.log.info("{any} (increased)", .{sum});
+fn should_increase(previous: ?u32, current: u32) bool {
+    if (previous == null) {
+        std.log.info("{any} (NA)", .{current});
+    } else if (previous.? < current) {
+        std.log.info("{any} (increased)", .{current});
         return true;
-    } else if (previous_sum.? == sum) {
-        std.log.info("{any} (same)", .{sum});
+    } else if (previous.? == current) {
+        std.log.info("{any} (same)", .{current});
     } else {
-        std.log.info("{any} (decreased)", .{sum});
+        std.log.info("{any} (decreased)", .{current});
     }
     return false;
 }
